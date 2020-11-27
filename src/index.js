a9b1dc8c25c136e00439b50b71a09f8ef4828a45
Corey Larson <coreyleelarson@gmail.com>
2020-11-27 16:02:34 -0600

    Adds support for Webpack 5.

--- src/index.js
+++ src/index.js
@@ -10,7 +10,19 @@ export function WebpackPnpExternals(options={}) {
             throw err;
         pnpApi = null;
     }
-    return (context, request, callback) => {
+    return function() {
+        const arg1 = arguments[0];
+        const arg2 = arguments[1];
+        const arg3 = arguments[2];
+        let context = arg1;
+        let request = arg2;
+        let callback = arg3;
+        // in case of webpack 5
+        if (arg1 && arg1.context && arg1.request) {
+            context = arg1.context;
+            request = arg1.request;
+            callback = arg2;
+        }
         if(pnpApi == null)
             return callback();
         //don't check relative or absolute requires
